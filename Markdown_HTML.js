e9f401a06df35cbd965f3c8b8bfa6b275ae2571e
Clark Hensley <clarkhensley@protonmail.com>
2023-04-12 23:30:02 -0500

    See example Markdown_HTML.js and Markdown_MD.js for comparisons of what does or doesn't work

---
+++ Markdown_HTML.js
@@ -0,0 +1,118 @@
+// <p>In this file, I will test as many markdown things as I can, to ensure that
+//     the conversion works correctly:<br><br><strong>Bold Text</strong></p>
+// <p><em>Italic Text</em></p>
+// <p>Left-Justified Text</p>
+// <p style="text-align: center;">Center-Justified Text</p>
+// <p style="text-align: right;">Right-Justified Text</p>
+// <p style="text-align: justify;">Fully-Justified Text. Obviously this one
+//     needs to be a bit longer. Lorem Ipsum dolor sit amet. The Quick Brown Fox
+//     Jumps Over The Lazy Dog. tHE qUICK bROWN fOX jUMPS oVER tHE lAZY dOG. foo
+//     bar baz qux quem etc.</p>
+// <p style="text-align: left;">&nbsp;</p>
+// <h1 style="text-align: left;">Big h1 Text</h1>
+// <h2 style="text-align: left;">h2 Text&nbsp;</h2>
+// <h3>h3 Text</h3>
+// <h4>h4 Text</h4>
+// <h5>h5 Text</h5>
+// <h6>h6 Text</h6>
+// <p><span style="text-decoration: underline;">Underlined Text</span></p>
+// <p><s><span style="text-decoration: underline;">Strikethrough Text</span></s>
+// </p>
+// <p>&nbsp;</p>
+// <p>Text in&nbsp;<sup>superscript</sup> and&nbsp;<sub>subscript</sub></p>
+// <p>&nbsp;</p>
+// <p><code>for good measure, here's a code-block</code></p>
+// <blockquote>
+//     <p>Blockquote Text</p>
+// </blockquote>
+// <div>Div Text</div>
+// <div>&nbsp;</div>
+// <p>Pre Text<br><br>Formatting Time:</p>
+// <p><span style="font-family: 'times new roman', times, serif;">Different
+//         Fonts</span></p>
+// <p><span style="font-family: 'comic sans ms', sans-serif;">Fonts</span></p>
+// <p><span style="font-family: wingdings, 'zapf dingbats';">Fonts</span></p>
+// <p><span style="font-size: 8pt;">tiny font</span></p>
+// <p><span style="font-size: 36pt;">LARGE FONT</span></p>
+// <p style="line-height: 2;">large line height</p>
+// <p style="line-height: 2;">like this all should be</p>
+// <p style="line-height: 2;">double spaced</p>
+// <p style="line-height: 1;">&nbsp;</p>
+// <p style="line-height: 1;"><span style="color: rgb(53, 152, 219);">Text in
+//         nice colors</span></p>
+// <p style="line-height: 1;">&nbsp;</p>
+// <p style="line-height: 1;"><span
+//         style="background-color: rgb(132, 63, 161); color: rgb(241, 196, 15);">Text
+//         in less-nice colors</span></p>
+// <p style="line-height: 1;">&nbsp;</p>
+// <p>A nice little table</p>
+// <table style="border-collapse: collapse; width: 100.024%;" border="1">
+//     <colgroup>
+//         <col style="width: 20.0122%;">
+//         <col style="width: 20.0122%;">
+//         <col style="width: 20.0122%;">
+//         <col style="width: 20.0122%;">
+//         <col style="width: 20.0122%;">
+//     </colgroup>
+//     <tbody>
+//         <tr>
+//             <td>a</td>
+//             <td>b</td>
+//             <td>c</td>
+//             <td>d</td>
+//             <td>e</td>
+//         </tr>
+//         <tr>
+//             <td>b</td>
+//             <td>c</td>
+//             <td>d</td>
+//             <td>e</td>
+//             <td>f</td>
+//         </tr>
+//         <tr>
+//             <td>c</td>
+//             <td>d</td>
+//             <td>e</td>
+//             <td>f</td>
+//             <td>g</td>
+//         </tr>
+//         <tr>
+//             <td>d</td>
+//             <td>e</td>
+//             <td>f</td>
+//             <td>g</td>
+//             <td>h</td>
+//         </tr>
+//         <tr>
+//             <td>e</td>
+//             <td>f</td>
+//             <td>g</td>
+//             <td>h</td>
+//             <td>i</td>
+//         </tr>
+//     </tbody>
+// </table>
+// <p><a href="https://github.com" target="_blank" rel="noopener">Here's a nice
+//         link</a></p>
+// <p>&nbsp;</p>
+// <p>&int; some unicode symbols &delta; &frac34;&nbsp;</p>
+// <p>&nbsp;</p>
+// <p>A line</p>
+// <hr>
+// <p>Embedded Video</p>
+// <p><iframe src="https://www.youtube.com/embed/Hp076_dxuVU" width="560"
+//         height="314" allowfullscreen="allowfullscreen"></iframe></p>
+// <p>&nbsp;</p>
+// <p>Some Emoji (more unicode)</p>
+// <p><img style="width: 1em; height: 1em; margin: 0 .05em 0 .1em; vertical-align: -.1em;"
+//         draggable="false"
+//         src="https://twemoji.maxcdn.com/v/13.0.1/72x72/1f4af.png" alt="💯"
+//         data-emoticon="true"> <img
+//         style="width: 1em; height: 1em; margin: 0 .05em 0 .1em; vertical-align: -.1em;"
+//         draggable="false"
+//         src="https://twemoji.maxcdn.com/v/13.0.1/72x72/1f171.png" alt="🅱️"
+//         data-emoticon="true"></p>
+// <p>&nbsp;</p>
+// <p>And some images:</p>
+// <p><img src="CodeChat1.png" alt=""><!-- pagebreak --></p>
+// <p><img src="CodeChat2.png" alt=""><img src="CodeChat3.png" alt=""></p>
