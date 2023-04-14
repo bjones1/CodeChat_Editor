6b095b9be5cb2607d40626cd807437f3e361ce5f
bjones1 <bjones@ece.msstate.edu>
2023-04-13 22:29:43 -0500

    Clean: Remove fixes for non-existant Ace editor code.

--- client/src/typings.d.ts
+++ client/src/typings.d.ts
@@ -1,21 +1,2 @@
 // <h1><code>typing.d.ts</code> &mdash; Missing type definitions</h1>
-// <p>This module keeps TypeScript from complaining about missing type
-//     definitions for Ace editor dynamic imports. See <a
-//         href="ace-webpack.mts">ace-webpack.mts</a>.</p>
-declare module "ace-code/src/theme/textmate";
-declare module "ace-code/src/mode/c_cpp";
-declare module "ace-code/src/mode/csharp";
-declare module "ace-code/src/mode/css";
-declare module "ace-code/src/mode/golang";
-declare module "ace-code/src/mode/java";
-declare module "ace-code/src/mode/javascript";
-declare module "ace-code/src/mode/json5";
-declare module "ace-code/src/mode/html";
-declare module "ace-code/src/mode/python";
-declare module "ace-code/src/mode/rust";
-declare module "ace-code/src/mode/sql";
-declare module "ace-code/src/mode/typescript";
-declare module "ace-code/src/mode/toml";
-declare module "ace-code/src/mode/verilog";
-declare module "ace-code/src/mode/yaml";
 declare module "graphviz-webcomponent/bundled";
