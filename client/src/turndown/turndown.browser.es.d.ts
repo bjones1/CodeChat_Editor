adf855279ddf9c8f6fcf5017ea0fb21855606375
Bryan A. Jones <bjones1@users.noreply.github.com>
2025-01-18 22:34:07 -0600

    Fix: Use Turndown's new word wrap instead of Prettier

--- client/src/turndown/turndown.browser.es.d.ts
+++ client/src/turndown/turndown.browser.es.d.ts
@@ -1,8 +1,13 @@
-// # `turndown.browser.es.d.ts` - TypeScript ignores for the Turndown library
+// `turndown.browser.es.d.ts` - TypeScript ignores for the Turndown library
+// ========================================================================
+//
 // This suppress type errors when using the Turndown library.
 declare class TurndownService {
     constructor(options: any);
-    use(_: any): any;
-    turndown(_: any): any;
+    use(_: Function|Array<Function>): any;
+    turndown(_: string|HTMLElement): string;
+    next(_: string|HTMLElement): string;
+    last(_: string|HTMLElement): string;
+    options: {[name: string]: any}
 }
 export default TurndownService;
