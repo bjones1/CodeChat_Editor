a2b55487bebbfa1a69a658c10b93e2456c91e181
Bryan A. Jones <bjones1@users.noreply.github.com>
2025-03-08 21:52:30 -0600

    Fix: the Update message handles more than just CodeChat Editor files.

--- server/src/webserver/vscode.rs
+++ server/src/webserver/vscode.rs
@@ -42,8 +42,8 @@ use super::{
 use crate::{
     oneshot_send,
     processing::{
-        CodeChatForWeb, CodeMirror, TranslationResultsString, codechat_for_web_to_source,
-        source_to_codechat_for_web_string,
+        CodeChatForWeb, CodeMirror, SourceFileMetadata, TranslationResultsString,
+        codechat_for_web_to_source, source_to_codechat_for_web_string,
     },
     queue_send,
     webserver::{
@@ -389,21 +389,48 @@ pub async fn vscode_ide_websocket(
                                                 // Translate the file.
                                                 let (translation_results_string, _path_to_toc) =
                                                 source_to_codechat_for_web_string(&contents.source.doc, &current_file, false);
-                                                if let TranslationResultsString::CodeChat(cc) = translation_results_string {
-                                                    // Send the new contents
-                                                    debug!("Sending translated contents to Client.");
-                                                    queue_send!(to_client_tx.send(EditorMessage {
-                                                        id: ide_message.id,
-                                                        message: EditorMessageContents::Update(UpdateMessageContents {
-                                                            file_path: clean_file_path.to_str().expect("Since the path started as a string, assume it losslessly translates back to a string.").to_string(),
-                                                            contents: Some(cc),
-                                                            cursor_position: None,
-                                                            scroll_position: None,
-                                                        }),
-                                                    }));
-                                                    Ok(ResultOkTypes::Void)
-                                                } else {
-                                                    Err("Error translating source to CodeChat.".to_string())
+                                                match translation_results_string {
+                                                    TranslationResultsString::CodeChat(cc) => {
+                                                        // Send the new translated contents.
+                                                        debug!("Sending translated contents to Client.");
+                                                        queue_send!(to_client_tx.send(EditorMessage {
+                                                            id: ide_message.id,
+                                                            message: EditorMessageContents::Update(UpdateMessageContents {
+                                                                file_path: clean_file_path.to_str().expect("Since the path started as a string, assume it losslessly translates back to a string.").to_string(),
+                                                                contents: Some(cc),
+                                                                cursor_position: None,
+                                                                scroll_position: None,
+                                                            }),
+                                                        }));
+                                                        Ok(ResultOkTypes::Void)
+                                                    }
+                                                    TranslationResultsString::Err(err) => Err(format!("Error translating source to CodeChat: {err}").to_string()),
+                                                    TranslationResultsString::Unknown => {
+                                                        // Send the new raw contents.
+                                                        debug!("Sending translated contents to Client.");
+                                                        queue_send!(to_client_tx.send(EditorMessage {
+                                                            id: ide_message.id,
+                                                            message: EditorMessageContents::Update(UpdateMessageContents {
+                                                                file_path: clean_file_path.to_str().expect("Since the path started as a string, assume it losslessly translates back to a string.").to_string(),
+                                                                contents: Some(CodeChatForWeb {
+                                                                    metadata: SourceFileMetadata {
+                                                                        // Since this is raw data, `mode` doesn't matter.
+                                                                        mode: "".to_string()
+                                                                    },
+                                                                    source: CodeMirror {
+                                                                        doc: contents.source.doc.clone(),
+                                                                        doc_blocks: vec![]
+                                                                    }
+                                                                }),
+                                                                cursor_position: None,
+                                                                scroll_position: None,
+                                                            }),
+                                                        }));
+                                                        Ok(ResultOkTypes::Void)
+                                                    },
+                                                    TranslationResultsString::Toc(_) => {
+                                                        Err("Error: source incorrectly recognized as a TOC.".to_string())
+                                                    }
                                                 }
                                             }
                                         }
