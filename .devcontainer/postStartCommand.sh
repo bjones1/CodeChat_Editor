05148210a1dab94027f3e274d238ef6ee9c9d392
Bryan A. Jones <bjones1@users.noreply.github.com>
2025-09-09 13:48:23 -0500

    Fix: tweak CodeSpaces config.

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-
-cd server
-# TODO: this doesn't open a file from the codespace, unfortunately.
-cargo run -- start ../README.md
+#
+# Currently, nothing needs to be done after the build completes.
