c5f7a61b034a6db5790f1828db65a268df13030e
Bryan A. Jones <bjones1@users.noreply.github.com>
2025-12-05 15:33:31 -0600

    Fix: add tests for cursor movement.

---
+++ server/tests/fixtures/overall/overall_core/test_4/test.py
@@ -0,0 +1,3 @@
+# The contents of this file don't matter -- tests will supply the content,
+# instead of loading it from disk. However, it does need to exist for
+# `canonicalize` to find the correct path to this file.
