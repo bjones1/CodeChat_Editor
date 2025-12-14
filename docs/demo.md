17ffdaa6678b898691605fbc5caab978e50c3f12
Bryan A. Jones <bjones1@users.noreply.github.com>
2025-12-14 08:37:10 -0600

    Add: an example of using literate programming.

---
+++ docs/demo.md
@@ -0,0 +1,27 @@
+`demo.md` - A script for a video demonstration of the CodeChat Editor
+================================================================================
+
+1. Introduction - start with `translation.rs`.
+   1. Point out TOC.
+   2. Show that comments are treated as Markdown and rendered.
+   3. Make edits on both sides.
+   4. Show sync through hyperlinks and changing files; show sync when selecting
+      lines.
+   5. Other features: images, hyperlinks, diagrams.
+2. Installation -- as a VSCode installation (preferred) or as a command-line
+   application from the
+   [releases](https://github.com/bjones1/CodeChat_Editor/releases) page.
+3. Running -- start the standalone app; switch to focus on the VSCode app.
+4. Use
+   1. Open some source code -- show the list of supported file types in the
+      readme.
+   2. Talk about comment formatting (one space after)
+   3. Format a document: hyperlink, headings, bold, monospace, code blocks, etc.
+   4. Insert an image (for example, draw.io)
+   5. Create a Mermaid diagram and a Graphviz diagram.
+   6. Write an equation.
+   7. Add a TOC and a readme.
+   8. Talk about style -- document function parameters,
+
+TODO: I'd like a good project that needs all these items. Something simple.
+Perhaps a programming challenge?
---
+++ examples/no-spoon/no_spoon.py
@@ -0,0 +1,97 @@
+# `no_spoon.py` - a CodinGame challenge
+# ==============================================================================
+#
+# This file demonstrates the use of literate programming in solving the
+# ["There is no Spoon"](https://www.codingame.com/ide/puzzle/there-is-no-spoon-episode-1)
+# programming challenge.
+#
+# Input
+# ------------------------------------------------------------------------------
+#
+# First, read the input provided by the challenge:
+#
+# The number of cells on the X axis.
+width = int(input())
+# The number of cells on the Y axis.
+height = int(input())
+# Read the grid of cells:
+#
+# * Each line is `width` characters.
+# * The characters are either `0` (occupied) or `.` (empty).
+#
+# From the website, here's an example grid:
+#
+# <img src="example_grid.png" alt="" width="50%" height="50%">
+#
+# The text which creates this grid is:
+#
+# ```
+# 00
+# 0.
+# ```
+#
+# Store this an an array of lines; each lines contains these characters.
+# Therefore, `line[y][x]` gives the cell at the coordinate $(𝑥,𝑦)$ (note the
+# reversed order).
+line = []
+for y in range(height):
+    line.append(input())
+
+# Processing and output
+# ------------------------------------------------------------------------------
+#
+# From the rules:
+#
+# > To do this, you must find each (x1,y1) coordinates containing a node, and
+# > display the (x2,y2) coordinates of the next node to the right, and the
+# > (x3,y3) coordinates of the next node to the bottom within the grid.
+# >
+# > If a neighbor does not exist, you must output the coordinates -1 -1 instead
+# > of (x2,y2) and/or (x3,y3).
+#
+# ### Approach
+#
+# Terminology:
+#
+# * A cell is one point in the grid. It may be empty or occupied.
+# * A node is an occupied cell.
+#
+# Variable naming: based on the rules, define:
+#
+# * (`x1`, `y1`) is the coordinate of an (occupied) node.
+# * (`x2`, `y2`) is the coordinate of the next node to the right.
+# * (`x3`, `y3`) is the coordinates of the next node to the bottom.
+#
+# ### Implementation
+#
+# * Loop through each cell. If it's occupied (a node):
+#
+#   * Look for a node to its right; if not found, return coorindates of
+#     $(-1,-1)$.
+#   * Look for a node to the bottom; if not found, return coorindates
+#     of $(−1,−1)$.
+for x1 in range(width):
+    for y1 in range(height):
+        if line[y1][x1] == "0":
+            # This cell is occupied. First, search for the next node to the
+            # right (note the `+ 1`) with the same y coordinate but a greater x
+            # coordinate:
+            y2 = y1
+            for x2 in range(x1 + 1, width):
+                if line[y2][x2] == "0":
+                    break
+            else:
+                # This runs only if we didn't find an occupied node.
+                x2 = -1
+                y2 = -1
+
+            # Do the same thing, but along the y axis.
+            x3 = x1
+            for y3 in range(y1 + 1, height):
+                if line[y3][x3] == "0":
+                    break
+            else:
+                x3 = -1
+                y3 = -1
+
+            print(f"{x1} {y1} {x2} {y2} {x3} {y3}")
